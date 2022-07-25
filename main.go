e88a25188cbfda5058957eb637f63dc115480c9d
Peter Stanko <peter.stanko@cloudtalk.io>
2022-07-25 15:44:01 +0200

    Add initial project structure with example data

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/pestanko/gothy-mini/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ pkg/cfg/app_config.go
@@ -0,0 +1,57 @@
+package cfg
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	// EnvProd represents the environment for production
+	EnvProd = "prod"
+	// EnvDev represents the environment for development
+	EnvDev = "dev"
+	// EnvStage represents the environment for staging
+	EnvStage = "stage"
+)
+
+// Application application configuration
+type Application struct {
+	Server ServerConfig `yaml:"server"`
+	Data   DataConfig   `yaml:"data"`
+}
+
+// ServerConfig holds configuration information for the application http server
+type ServerConfig struct {
+	Addr string `yaml:"addr"`
+}
+
+// DataConfig holds configuration information for which data files should be loaded
+type DataConfig struct {
+	Load []string `yaml:"load"`
+}
+
+// LoadApplicationConfig for the spec. environment
+func LoadApplicationConfig(environment string) (app Application, err error) {
+	filepath := path.Join("config", fmt.Sprintf("app.%s.yml", environment))
+
+	if err = loadYamlFile(filepath, &app); err != nil {
+		return app, err
+	}
+
+	return
+}
+
+func loadYamlFile(filepath string, data interface{}) error {
+	content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return fmt.Errorf("unable to load configuration file '%s': %s", filepath, err)
+	}
+
+	if err = yaml.Unmarshal(content, data); err != nil {
+		return fmt.Errorf("unable to parse configuration file '%s': %s", filepath, err)
+	}
+	return nil
+}
